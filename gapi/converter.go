eb56cec6b1d40286e283ec791821ab8342799abe
Kim Jinyoung <sdk159147@gmail.com>
2022-09-27 06:07:40 +0900

    Implement gRPC server RPC call CreateUser LoginUser

---
+++ gapi/converter.go
@@ -0,0 +1,18 @@
+package gapi
+
+import (
+	db "github.com/jiny0x01/simplebank/db/sqlc"
+	"github.com/jiny0x01/simplebank/pb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func convertUser(user db.Users) *pb.User {
+	return &pb.User{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: timestamppb.New(user.PasswordChangedAt),
+		CreatedAt:         timestamppb.New(user.CreatedAt),
+	}
+}
+
